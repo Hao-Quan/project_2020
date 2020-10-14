3bf7465b7e01bfce05a8a8e25d2530fe25efaf67
hao <hao.quan@icloud.com>
2020-10-14 17:28:43 +0200

    Plot confusion matrix

--- research/research_distance_SGN_new/ensemble/ensemble.py
+++ research/research_distance_SGN_new/ensemble/ensemble.py
@@ -15,59 +15,62 @@ if __name__ == "__main__":
     right_num = total_num = right_num_5 = 0
 
 
-    # alpha = 1
-    # for i in range(len(label) - 7):
-    #     # _, l = label[:, i]
-    #     # _, r11 = list(r1.items())[i]
-    #     # _, r22 = list(r2.items())[i]
-    #     # r = r11 + r22 * arg.alpha
-    #     #
-    #     l = label[i]
-    #     r11 = r1[i]
-    #     r22 = r2[i]
-    #     r = alpha * r11 + r22*0
-    #     # r = r11
-    #     rank_5 = r.argsort()[-5:]
-    #     right_num_5 += int(int(l) in rank_5)
-    #     r = np.argmax(r)
-    #     right_num += int(r == int(l))
-    #     total_num += 1
-    # acc = right_num / total_num
-    # acc5 = right_num_5 / total_num
+    alpha = 22
+    list_pred = []
+    for i in range(len(label) - 7):
+        l = label[i]
+        r11 = r1[i]
+        r22 = r2[i]
+        r = alpha * r11 + r22
+        rank_5 = r.argsort()[-5:]
+        right_num_5 += int(int(l) in rank_5)
+        r = np.argmax(r)
+        right_num += int(r == int(l))
+        total_num += 1
+
+        # append predict label in list for Confusion matrix
+        list_pred.append(r)
+
+    acc = right_num / total_num
+    acc5 = right_num_5 / total_num
+
+    print('Top1 Acc: {:.4f}%'.format(acc * 100))
+    print('Top5 Acc: {:.4f}%'.format(acc5 * 100))
+
+    with open('./xsub/xsub_predict_label.npy', 'wb') as f:
+        print("save ./xsub/xsub_predict_label.npy")
+        np.save(f, list_pred)
+
+
+    # best_acc = -1
+    # best_alpha = -1
     #
-    # print('Top1 Acc: {:.4f}%'.format(acc * 100))
-    # print('Top5 Acc: {:.4f}%'.format(acc5 * 100))
-
-
-    best_acc = -1
-    best_alpha = -1
-
-    for alpha in np.arange(0.5,50,0.5):
-        for i in range(len(label)-7):
-            # _, l = label[:, i]
-            # _, r11 = list(r1.items())[i]
-            # _, r22 = list(r2.items())[i]
-            # r = r11 + r22 * arg.alpha
-            #
-            l = label[i]
-            r11 =r1[i]
-            r22 = r2[i]
-            r = alpha*r11 + r22
-            #r = r11
-            rank_5 = r.argsort()[-5:]
-            right_num_5 += int(int(l) in rank_5)
-            r = np.argmax(r)
-            right_num += int(r == int(l))
-            total_num += 1
-        acc = right_num / total_num
-        acc5 = right_num_5 / total_num
-
-        if acc > best_acc:
-            best_acc = acc
-            best_alpha = alpha
-
-        print('Alpha: {:.4f}%'.format(alpha))
-        print('Top1 Acc: {:.4f}%'.format(acc * 100))
-        print('Top5 Acc: {:.4f}%'.format(acc5 * 100))
-
-    print("Best acc: {:.4f} with Alpha {:.2f}".format(best_acc, best_alpha))
+    # for alpha in np.arange(0.5,50,0.5):
+    #     for i in range(len(label)-7):
+    #         # _, l = label[:, i]
+    #         # _, r11 = list(r1.items())[i]
+    #         # _, r22 = list(r2.items())[i]
+    #         # r = r11 + r22 * arg.alpha
+    #         #
+    #         l = label[i]
+    #         r11 =r1[i]
+    #         r22 = r2[i]
+    #         r = alpha*r11 + r22
+    #         #r = r11
+    #         rank_5 = r.argsort()[-5:]
+    #         right_num_5 += int(int(l) in rank_5)
+    #         r = np.argmax(r)
+    #         right_num += int(r == int(l))
+    #         total_num += 1
+    #     acc = right_num / total_num
+    #     acc5 = right_num_5 / total_num
+    #
+    #     if acc > best_acc:
+    #         best_acc = acc
+    #         best_alpha = alpha
+    #
+    #     print('Alpha: {:.4f}%'.format(alpha))
+    #     print('Top1 Acc: {:.4f}%'.format(acc * 100))
+    #     print('Top5 Acc: {:.4f}%'.format(acc5 * 100))
+    #
+    # print("Best acc: {:.4f} with Alpha {:.2f}".format(best_acc, best_alpha))
